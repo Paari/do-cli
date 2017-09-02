16caf5f1f5ea3c763054f840fd50d1e3ad3e09fe
paritosh_pundir <0p0a0r0i@gmail.com>
2017-09-02 23:41:44 +0530

    Function to create a .tasks file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var chalk = require('chalk');
 var clear = require('clear');
 var figlet = require('figlet');
+var _ = require('lodash');
+var fs = require('fs');
+var touch = require('touch');
 
 clear();
 console.log(
@@ -8,3 +11,9 @@ console.log(
     figlet.textSync('DO CLI', { horixontalLayout: 'full' })
   )
 )
+
+function createFile () {
+  touch( '.tasks' );
+}
+
+createFile()
